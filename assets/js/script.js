6c8e59e4515a77fb1fdbbcaa5186935d8ef0245e
Elaine Phan <elainep926@gmail.com>
2019-07-05 12:22:33 -0700

    Added simple HTML skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
+$(document).ready(initializeApp);
+var firstCardClicked={'card-back':null, 'value':null};
+var secondCardClicked={'card-back':null, 'value':null};
+var matches=null;
 
+function initializeApp(){
+    $(".lfz-card").click(handleCardClick);
+    
+}
+
+function handleCardClick(event){
+    if ( firstCardClicked.card == null){
+        firstCardClicked.card = event.currentTarget;
+        $(firstCardClicked.card).addClass("hidden");
+    } else if ( firstCardClicked.card != null && secondCardClicked.card == null){
+        secondCardClicked.card= event.currentTarget;
+        $(secondCardClicked.card).addClass("hidden");
+    } else {
+        // var card =event.currentTarget;
+        // console.log(card);
+        $(firstCardClicked.card).removeClass("hidden");
+        firstCardClicked.card=null;
+        $(secondCardClicked.card).removeClass("hidden");
+        secondCardClicked.card=null;
+    }
+
+}
+
+// var firstCardClicked=null;
+//         var secondCardClicked=null;
+//         var matches=null;
+
+//         if(firstCardClicked===null){
+//             firstCardClicked=$(event.currentTarget);
+//         }else if(firstCardClicked!==null){
+//             secondCardClicked=$(event.currentTarget);
+//         }
+
+//         if(firstCardClicked!==null&&secondCardClicked!==null){
+//             firstCardClicked=$()
+//         }
