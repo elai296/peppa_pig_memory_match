18b8ea2c171fb368bbf276724535a5d03da50fca
Elaine Phan <elainep926@gmail.com>
2019-07-08 20:49:46 -0700

    tracking-stats

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,62 +5,40 @@ var matches=null;
 var firstCardImage = null;
 var secondCardImage = null;
 var max_matches=9;
+var attempts= 0;
+var games_played=0;
+var accuracy=0;
 
 function initializeApp(){
     $(".lfz-card").click(handleCardClick);
-         
+    games_played+=1;
 }
 
-// function handleCardClick(event){
-    
-//     if(firstCardClicked===null){
-//         firstCardClicked=$(event.currentTarget);
-//         firstCardImage=$(firstCardClicked).siblings(".card-back").css("background-image");
-//         $(firstCardClicked).addClass("hidden");
-//     }else if(firstCardClicked!==null){
-//         secondCardClicked=$(event.currentTarget);
-//         secondCardImage=$(secondCardClicked).siblings(".card-back").css("background-image");
-//         $(secondCardClicked).addClass("hidden"); 
-//         if(firstCardImage===secondCardImage){
-//             console.log("they matched");
-//                 matches+=1;
-//                 // firstCardClicked=null;
-//                 // secondCardClicked=null;
-//             }else{
-//                 setTimeout(function(){ 
-//                    
-//                 $(firstCardClicked).removeClass("hidden");}, 2000);
-//                 setTimeout(function(){ 
-//                   
-//                 $(secondCardClicked).removeClass("hidden");}, 2000);
-//                 console.log("not matched");
-//                 // firstCardClicked=null;
-//                 // secondCardClicked=null;
-//             }
-//         }
-// }
 function handleCardClick(event){
 
-    if(firstCardClicked===null){ 
-      firstCardClicked=$(event.currentTarget);
-      firstCardImage=$(firstCardClicked).siblings(".card-back").css("background-image");
-      $(firstCardClicked).addClass("hidden");
-    }else if(firstCardClicked!==null){
-      secondCardClicked=$(event.currentTarget);
-      secondCardImage=$(secondCardClicked).siblings(".card-back").css("background-image");
-      $(secondCardClicked).addClass("hidden"); 
-      if(firstCardImage===secondCardImage){
-        console.log("they matched");
-        matches+=1;
-        if(matches===max_matches){
-          $(".modal").removeClass('hidden')
-          
+  if(firstCardClicked===null){ 
+    firstCardClicked=$(event.currentTarget);
+    firstCardImage=$(firstCardClicked).siblings(".card-back").css("background-image");
+    $(firstCardClicked).addClass("hidden");
+  }else if(firstCardClicked!==null){
+    secondCardClicked=$(event.currentTarget);
+    secondCardImage=$(secondCardClicked).siblings(".card-back").css("background-image");
+    $(secondCardClicked).addClass("hidden"); 
+    attempts+=1;
+    
+    if(firstCardImage===secondCardImage){
+      console.log("they matched");
+      matches+=1;
+      
+      if(matches===max_matches){
+        $(".modal").removeClass('hidden')
+        
         //   $(".modal").show();
         // }else{
-        //   $(".modal").hide
+          //   $(".modal").hide
         }
-      firstCardClicked=null;
-      secondCardClicked=null;
+        firstCardClicked=null;
+        secondCardClicked=null;
       }else{
         $(".lfz-card").off('click');
         setTimeout(function(){ 
@@ -73,4 +51,17 @@ function handleCardClick(event){
         console.log("not matched");
       }
     }
+    displayStats();
+  }
+
+  function calculateAccuracy(){
+    accuracy = matches / attempts;
+    return accuracy;
+  }
+
+  function displayStats(){
+    var resultAccuracy=calculateAccuracy();
+    $("#games-played").text(games_played);
+    $("#attempts").text(attempts);
+    $("#accuracy").text(resultAccuracy);
   }
